c61b2d56d8c35a078fba27786770600194d4e0ec
MAlexGG <alexandragalarzag@gmail.com>
2024-03-10 13:16:49 +0100

    passing a single object from the controller to the view

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,20 +1,27 @@
 package com.ltp.gradesubmission;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-
 @Controller
 public class GradeController {
 
+    List<Grade> studentGrades = new ArrayList<>();
+
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        Grade grade = new Grade("Harry", "Poisons", "C-");
-        model.addAttribute("grade", grade);
+        studentGrades.add(new Grade("Harry", "Poisons", "C-"));
+        studentGrades.add(new Grade("Hermiony", "Arithmetics", "A+"));
+        studentGrades.add(new Grade("Neville", "Charms", "A-"));
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
+
     
     
 }
