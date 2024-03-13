c8b6787a31115a5ee706644d2402f09516553c48
MAlexGG <alexandragalarzag@gmail.com>
2024-03-13 14:13:34 +0100

    update method passing name

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -14,24 +15,35 @@ public class GradeController {
 
     List<Grade> studentGrades = new ArrayList<>();
 
+    @GetMapping("/")
+    public String getForm(Model model, @RequestParam(required = false) String name){      
+        model.addAttribute("grade", getGradeIndex(name) == -1000 ? new Grade() : studentGrades.get(getGradeIndex(name)));
+        return "form";
+    }
+
     @GetMapping("/grades")
     public String getGrades(Model model) {
         model.addAttribute("grades", studentGrades);
         return "grades";
     }
-
-    @GetMapping("/")
-    public String getForm(Model model){
-        model.addAttribute("grade", new Grade());
-        return "form";
-    }
     
     @PostMapping("/handleSubmit")
     public String submitForm(Grade grade){
-        studentGrades.add(grade);
+        int index = getGradeIndex(grade.getName());
+        if(index == -1000){
+            studentGrades.add(grade);
+        } else {
+            studentGrades.set(index, grade);
+        }
         return "redirect:/grades";
     }
 
+    public Integer getGradeIndex(String name){
+        for(int i = 0; i < studentGrades.size(); i++){
+            if (studentGrades.get(i).getName().equals(name)) return i;
+        } 
+        return -1000;
+    }
     
     
 }
